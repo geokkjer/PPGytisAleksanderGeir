5ef524c9e2392295e21005a6dd4c06b0b46d3563
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-12-18 13:02:24 +0100

    Initi

---
+++ Nissens_liste_og_hans_hjelpere/Nissens_liste_og_hans_hjelpere/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nissens_liste_og_hans_hjelpere
+{
+    internal class Helper
+    {
+    }
+}
